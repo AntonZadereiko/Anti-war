fb76c11dbcaa30c7527139682a6ab1cbc9ae924a
AntonZadereiko <wieze@ukr.net>
2023-04-11 23:11:28 +0300

    add burger

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         autoplayTimeout:2000,
         responsive:{
             0:{
-                items:2
+                items:1
             },
 
             1000:{
